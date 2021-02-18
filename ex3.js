fb8266dc63343d0571f044401da7915f995dfe61
TMFV <68297974+TMFV@users.noreply.github.com>
2021-02-18 15:37:28 +0200

    ex3
    finish ex3

---
+++ ex3.js
@@ -0,0 +1,50 @@
+//Задание 3
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const makeTransaction = (transaction) => {
+    
+    return new Promise((resolve, reject) => {
+        const delay = randomIntegerFromInterval(200, 500);
+        setTimeout(() => {
+            let iD = transaction.id;
+            const canProcess = Math.random() > 0.3;
+            if (canProcess) {
+                let time = delay;
+                console.log(time);
+                resolve([iD, time]);
+            } else {
+                reject(iD);
+            }
+        }, delay);
+    });
+};
+
+
+const logSuccess = (arrayResolve) => {
+  console.log(`Transaction ${arrayResolve[0]} processed in ${arrayResolve[1]}ms`);
+};
+
+const logError = id => {
+  console.warn(`Error processing transaction ${id}. Please try again later.`);
+};
+
+/*
+ // Должно работать так
+ */
+makeTransaction({ id: 70, amount: 150 })
+  .then(logSuccess)
+  .catch(logError);
+
+makeTransaction({ id: 71, amount: 230 })
+  .then(logSuccess)
+  .catch(logError);
+
+makeTransaction({ id: 72, amount: 75 })
+  .then(logSuccess)
+  .catch(logError);
+
+makeTransaction({ id: 73, amount: 100 })
+  .then(logSuccess)
+    .catch(logError);
